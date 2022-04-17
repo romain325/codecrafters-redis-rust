a5a493513e20d714ba08002585671d10cb279d8d
romain325 <romainolivier42@gmail.com>
2022-04-17 15:20:06 +0200

    bind port

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
-#[allow(unused_imports)]
-use std::net::TcpListener;
+use tokio::net::TcpListener;
+use std::error::Error;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    // match listener.accept() {
-    //     Ok((_socket, addr)) => println!("accepted new client: {:?}", addr),
-    //     Err(e) => println!("couldn't accept client: {:?}", e),
-    // }
+    let mut listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        println!("New connection from {}", addr);
+    }
+
 }
